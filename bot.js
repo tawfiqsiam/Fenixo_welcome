3efe6e66cb3d5fd05bd0225a9c7dfe6d7147ba21
NerLOR#0001 <36960271+mraco@users.noreply.github.com>
2018-10-31 22:51:02 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,15 @@ const fs = require('fs');
 const moment = require('moment');
 const jimp = require('jimp');
 const Canvas = require('canvas');
-
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`By : lg server.`);
+        console.log(`---------------`);
+      console.log(`ON ${client.guilds.size} Servers `);
+    console.log(`---------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setGame(`lg server`);
+}); // 
 var dat = JSON.parse("{}");
 function forEachObject(obj, func) {
     Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
