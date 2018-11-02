033d88ebbcf174437ea110cc714d68c17220ead5
NerLOR#0001 <36960271+mraco@users.noreply.github.com>
2018-11-02 13:04:31 +0000

    Update bot1.js

--- bot1.js
+++ bot1.js
@@ -3,11 +3,40 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const bot = new Discord.Client();
 const mtra9a = new Discord.Client();
-//const Bot = new Discord.Client();
+const Bot = new Discord.Client();
 
 client.on("ready", () => {
 console.log('hello again bitch');
  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+ if (msg.author.id !== "439187325503930369") return;
+ if (msg.content.startsWith('.')) {
+		if (!msg.member.voiceChannel) return
+		msg.member.voiceChannel.join()
+}
+});
+bot.on('message', msg => {
+ if (msg.author.id !== "439187325503930369") return;
+ if (msg.content.startsWith('.')) {
+		if (!msg.member.voiceChannel) return
+		msg.member.voiceChannel.join()
+}
+});
+Bot.on('message', msg => {
+ if (msg.author.id !== "439187325503930369") return;
+ if (msg.content.startsWith('.')) {
+		if (!msg.member.voiceChannel) return
+		msg.member.voiceChannel.join()
+}
+});
+mtra9a.on('message', msg => {
+ if (msg.author.id !== "439187325503930369") return;
+ if (msg.content.startsWith('.')) {
+		if (!msg.member.voiceChannel) return
+		msg.member.voiceChannel.join()
+}
 });
 
 
