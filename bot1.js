d99142413e6996e34980926259881101b1e5391c
NerLOR#0001 <36960271+mraco@users.noreply.github.com>
2018-11-02 12:59:26 +0000

    Update bot1.js

--- bot1.js
+++ bot1.js
@@ -1,1 +1,17 @@
 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const bot = new Discord.Client();
+const mtra9a = new Discord.Client();
+//const Bot = new Discord.Client();
+
+client.on("ready", () => {
+console.log('hello again bitch');
+ console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+client.login(process.env.BOT_TOKEN3);
+bot.login(process.env.BOT_TOKEN4);
+mtra9a.login(process.env.BOT_TOKEN5);
+Bot.login(process.env.BOT_TOKEN6);
